52fcb4b898f205a5315f16c7c61aa9225a4a707d
sekar N <sekarnagarajan1234@gmail.com>
2023-11-16 21:27:12 +0530

    java

---
+++ ArrayList/src/Program1.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+class Program1 {
+    public static void main(String[] args) {
+        // Create a new ArrayList to store colors
+        ArrayList<String> colorsList = new ArrayList<>();
+
+        // Add some colors to the ArrayList
+        colorsList.add("Red");
+        colorsList.add("Green");
+        colorsList.add("Blue");
+        colorsList.add("Yellow");
+        colorsList.add("Orange");
+
+        // Print out the collection
+        System.out.println("Colors in the ArrayList:");
+        for (String color : colorsList) {
+            System.out.println(color);
+        }
+    }
+}
