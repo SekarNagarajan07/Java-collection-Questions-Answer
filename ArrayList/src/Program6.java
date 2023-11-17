09ce52e1184b044fdd3bbe2d5b5d0f39dc788b2e
sekar N <sekarnagarajan1234@gmail.com>
2023-11-17 23:32:37 +0530

    java

---
+++ ArrayList/src/Program6.java
@@ -0,0 +1,28 @@
+//Write a Java program to remove the third element from an array list.
+
+import java.util.ArrayList;
+
+public class Program6 {
+    public static void main(String[] args) {
+        // Creating an ArrayList
+        ArrayList<String> arrayList = new ArrayList<>();
+
+        // Adding elements to the ArrayList
+        arrayList.add("One");
+        arrayList.add("Two");
+        arrayList.add("Three");
+        arrayList.add("Four");
+        arrayList.add("Five");
+
+        // Displaying the original ArrayList
+        System.out.println("Original ArrayList: " + arrayList);
+
+        // Removing the third element (index 2)
+        if (arrayList.size() >= 3) {
+            arrayList.remove(2);
+            System.out.println("ArrayList after removing the third element: " + arrayList);
+        } else {
+            System.out.println("ArrayList does not have a third element to remove.");
+        }
+    }
+}
